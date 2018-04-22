8bd635fbb14c5771b362404fd7616df1fcd1f606
Sophia0616 <37453758+Sophia0616@users.noreply.github.com>
2018-04-21 19:52:12 -0500

    Update index.vue

--- components/index.vue
+++ components/index.vue
@@ -18,12 +18,12 @@
 	display: flex;
 	align-items: center;
 	justify-content: center;
+	margin-right: .25rem;
+	line-height: 1;
+	width: 0.875rem;
+	vertical-align: baseline;
 	svg {
 		font-size: 0.875rem;
-		margin-right: .25rem;
-		line-height: 1;
-		width: 0.875rem;
-		vertical-align: baseline;
    		display: inline-block;
 	}
 }
